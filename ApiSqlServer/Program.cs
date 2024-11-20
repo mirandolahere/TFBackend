a3b7baa6436b675beeb4616734c987633957f36a
Claudia Alexandra Miranda Chillitupa <miranda.95.cm@gmail.com>
2024-11-20 17:55:41 -0500

    cambios

--- ApiSqlServer/Program.cs
+++ ApiSqlServer/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
 
